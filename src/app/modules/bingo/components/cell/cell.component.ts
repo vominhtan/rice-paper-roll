70040a0549a727c5a2d05ad3b3b91153ca5bc132
Tân Võ Minh <vominhtan1988@gmail.com>
2019-12-31 18:49:03 +0700

    Add cell event

--- src/app/modules/bingo/components/cell/cell.component.ts
+++ src/app/modules/bingo/components/cell/cell.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'rpr-cell',
+  selector: CellComponent.selector,
   templateUrl: './cell.component.html',
   styleUrls: ['./cell.component.scss']
 })
 export class CellComponent {
 
+  static readonly selector = 'rpr-cell';
   @Input() cellNumber: number;
+  @Input() cellIndex: number;
+  @Output() check: EventEmitter<any> = new EventEmitter();
   checked = false;
 
   constructor() { }
 
   toggle() {
     this.checked = !this.checked;
+    if (this.checked) {
+      this.check.emit(this.checked);
+    }
   }
 
 }
--- src/app/modules/bingo/components/row/row.component.ts
+++ src/app/modules/bingo/components/row/row.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'rpr-row',
+  selector: RowComponent.selector,
   templateUrl: './row.component.html',
   styleUrls: ['./row.component.scss'],
 })
 export class RowComponent {
+
+  static readonly selector = 'rpr-row';
+
   @Input() row: number[];
 
   constructor() {}
+
+  cellCheck(index: number, cellNumber: number, checked: boolean) {
+    console.log(arguments);
+  }
 }
